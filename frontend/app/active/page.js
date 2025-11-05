6bd8a1055d5af0d9ae2f6f66c58b3d1cf8b33fb5
David Cheng <chengdavid10917@gmail.com>
2025-11-05 15:47:59 -0500

    feat: create /active page

--- frontend/app/active/page.js
+++ frontend/app/active/page.js
@@ -1,38 +1,53 @@
 import styles from "./page.module.css";
 
-export default function Active(){
+export default async function Active(){
+    // const response = await fetch('http://localhost:5000/active');
+    const response = await fetch(`${process.env.API_URL}/active`);
+    const active = await response.json();
+    // console.log(active)
+
     return(
         <div>
             <h1 className={styles.activeHead}>Active Roster</h1>
             
             <div className={styles.graybox}>
                 <h2 className={styles.activeHeader2}>Executive Board</h2>
-
-                <div className={styles.itemPres}>
-                    <h3 className={styles.activeHeader3}>President</h3>
-                    <div>Adam "Joker" Prince</div>
-                </div>
-
                 <div className={styles.box}>
-                    {executive_positions.map((item) => (
-                        <PositionExec key={item.role} {...item} />   
-                    ))}
+                    {active.map((Active) => {
+                        const key=`${Active.pos_id}`
+                        const title = `${Active.title}`
+                        const brother_i = `${Active.bro_i.Firstname} "${Active.bro_i.Nickname}" ${Active.bro_i.Lastname}`
+                        if (Active.executive)
+                            return (
+                                <PositionExec key={key} role={title} bro={brother_i}/>
+                            )
+                    })}
                 </div>
             </div>
 
             <div className={styles.whitebox}>
                 <h2 className={styles.activeHeader2}>Minor Board</h2>
                 <div className={styles.box}>
-                    {minor_positions.map((item) => (
-                        <PositionMinor key={item.role} {...item} />   
-                    ))}
-                </div>
+                    {active.map((Active) => {
+                        const key=`${Active.pos_id}`
+                        const title = `${Active.title}`
+                        const brother_i = `${Active.bro_i.Firstname} "${Active.bro_i.Nickname}" ${Active.bro_i.Lastname}`
+                        let brother_ii = null
+                        let brother_iii = null
 
+                        if (Active.bro_ii?.Number > 0){brother_ii = `${Active.bro_ii.Firstname} "${Active.bro_ii.Nickname}" ${Active.bro_ii.Lastname}`}
+                        if (Active.bro_iii?.Number > 0){brother_iii = `${Active.bro_iii.Firstname} "${Active.bro_iii.Nickname}" ${Active.bro_iii.Lastname}`}
+                        if (Active.pos_id < 20 && !Active.executive)
+                            return (
+                                <PositionMinor key={key} role={title} first={brother_i} second={brother_ii} third={brother_iii}/>
+                            )
+                    })}
+                </div>
             </div>
-          
         </div>
     );
 }
+
 function PositionExec({role, bro}){
     return(
         <div className={styles.itemExec}>
@@ -41,6 +56,7 @@ function PositionExec({role, bro}){
         </div>
     )
 }
+
 function PositionMinor({role, first, second, third}){
     return(
         <div className={styles.itemMinor}>
@@ -52,29 +68,3 @@ function PositionMinor({role, first, second, third}){
     )
 }
 
-const executive_positions = [
-    { role: 'External VP',  bro: 'Jason "Mach 8" Hungreder'},
-    { role: 'Internal VP',  bro: 'Gianni "Maverick" Bonval'},
-    { role: 'Secretary',    bro: 'Ryan "Wicked Games" Cheung'},
-    { role: 'Treasurer',    bro: 'Stanley "Regulus" Nguyen'}
-]
-
-const minor_positions = [
-    { role: 'Academic',                 first: 'Ryan "Wicked Games" Cheung',    second: '', third:''},
-    { role: 'Brotherhood',              first: 'Greg "Apex" Dunne',             second: 'Jayson "Résonnant Vie" You', third:''},
-    { role: 'Social',                   first: 'Greg "Apex" Dunne',             second: 'Zack "Vox" Neville', third:''},
-    { role: 'Philanthropy',             first: 'Greg "Apex" Dunne',             second: '', third:''},
-    { role: 'Fundraiser',               first: 'Nao "Vertex" Tomita',           second: 'Chen Hui "Zenith" Li', third:''},
-    { role: 'Service Chair',            first: 'Zack "Vox" Neville',                      second: '', third: ''},
-    { role: 'Risk Management',          first: 'Stanley "Regulus" Nguyen',      second: '', third:''},
-    { role: 'Public Relations',         first: 'Gianni "Maverick" Bonval',      second: 'Zack "Vox" Neville', third:''},
-    { role: 'Performance',              first: 'Zack "Vox" Neville',            second: 'Adam "Joker" Prince', third: ''},
-    { role: 'Install Coordinator',      first: 'Chen Hui "Zenith" Li',          second: 'Ryan "Wicked Games" Cheung', third: 'Jayson "Résonnant Vie" You'},
-    { role: 'IGC Delegate',             first: 'Jayson "Résonnant Vie" You',    second: '', third: ''},
-    { role: 'PAC Rep',                  first: 'Nao "Vertex" Tomita',           second: '', third: ''},
-    { role: 'Cultural',                 first: 'Nao "Vertex" Tomita',           second: '', third: ''},
-    { role: 'Web Master',               first: 'Gianni "Maverick" Bonval',      second: 'Jayson "Résonnant Vie" You', third: ''}
-
-    // { col:'col3', role: 'Probate Chair',            first: 'LongName', second: '', third: ''},
-    // { col:'col3', role: 'Rush/Recruitment Chair',   first: 'LongName', second: '', third: ''}
-]
